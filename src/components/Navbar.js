73873fccf543820e5acf8a2531a2e44bf954dac3
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-18 09:53:32 +0600

    fix: updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
+import { Link } from "react-router-dom";
 import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import logoBig from "../assets/logo-big.svg";
+import logoSmall from "../assets/logo-small.svg";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "How", href: "/how", current: false },
   { name: "About", href: "/about", current: false },
 ];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Navbar() {
   return (
@@ -19,7 +19,7 @@ export default function Navbar() {
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* Mobile menu button*/}
-              <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-500 rounded-md hover:text-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-500 rounded-md hover:text-gray-900 focus:outline-non">
                 <span className="sr-only">Open main menu</span>
                 {open ? (
                   <XIcon className="block w-6 h-6" aria-hidden="true" />
@@ -28,57 +28,45 @@ export default function Navbar() {
                 )}
               </Disclosure.Button>
             </div>
-            <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-between">
-              <div className="flex items-center flex-shrink-0">
+            <div className="flex items-center justify-center flex-1 sm:justify-between">
+              <div className="flex flex-shrink-0">
                 <img
-                  className="block w-auto h-8 border border-red-500 lg:hidden"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="demo logo"
+                  className="block w-auto h-12 lg:hidden"
+                  src={logoSmall}
+                  alt="Happy Plant"
                 />
                 <img
-                  className="hidden w-auto h-8 border border-red-500 lg:block"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                  alt="demo logo"
+                  className="hidden w-auto h-12 lg:block"
+                  src={logoBig}
+                  alt="Happy Plant"
                 />
               </div>
-              <div className="hidden sm:block">
+              <div className="hidden -mr-2 sm:block">
                 <div className="flex space-x-5">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "text-gray-900"
-                          : "text-gray-400 hover:text-gray-900",
-                        "px-3 py-2 text-md font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
+                      to={item.href}
+                      className="px-3 py-2 font-medium text-gray-500 hover:text-gray-900 text-md"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="sm:hidden ">
             <div className="pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "text-gray-900"
-                      : "text-gray-400 hover:text-gray-900",
-                    "block px-3 py-2 text-md font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
+                  to={item.href}
+                  className="flex flex-row px-3 py-2 font-medium text-gray-500 hover:text-gray-900 text-md"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
