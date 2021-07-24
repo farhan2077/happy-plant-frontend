c366180fa4aba87e6d57380ed9cec6c48402b9b9
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-24 19:55:58 +0600

    new: updated big logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,35 +19,41 @@ export default function Home() {
   }, []);
 
   return (
-    <section className="grid h-full">
+    <>
       <MetaDecorator title="Happy Plant" />
-      <div className="m-auto text-center">
-        {plantData.success && plantData.data.length ? (
-          <div>
-            <div className="w-64 sm:w-80">
-              {plantData.data[0].reading < upperValue &&
-              plantData.data[0].reading > lowerValue ? (
-                <img
-                  src={happyPlant}
-                  alt="Happy plant"
-                  className="w-full h-auto -mt-32 sm:-mt-10"
-                />
-              ) : (
-                <img src={sadPlant} alt="Sad Plant" className="w-full h-auto" />
-              )}
+      <section className="grid h-full">
+        <div className="m-auto text-center">
+          {plantData.success && plantData.data.length ? (
+            <div>
+              <div className="w-64 sm:w-80">
+                {plantData.data[0].reading < upperValue &&
+                plantData.data[0].reading > lowerValue ? (
+                  <img
+                    src={happyPlant}
+                    alt="Happy plant"
+                    className="w-full h-auto -mt-32 sm:-mt-10"
+                  />
+                ) : (
+                  <img
+                    src={sadPlant}
+                    alt="Sad Plant"
+                    className="w-full h-auto"
+                  />
+                )}
+              </div>
+              <p className="mt-3 text-xl">
+                Soil moisture is {plantData.data[0].reading}
+              </p>
             </div>
-            <p className="mt-3 text-xl">
-              Soil moisture is {plantData.data[0].reading}
-            </p>
-          </div>
-        ) : (
-          <div className="mx-10">
-            Can not display plant status 😢.
-            <br />
-            Check if everything is working or try again later.
-          </div>
-        )}
-      </div>
-    </section>
+          ) : (
+            <div className="mx-10">
+              Can not display plant status 😢.
+              <br />
+              Check if everything is working or try again later.
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 }
