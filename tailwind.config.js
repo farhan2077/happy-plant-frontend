7990d7fa337dd0e857f5ab009516df1edfde1f3f
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-24 19:54:26 +0600

    new: added `@tailwindcss/typography`

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,33 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      orange: "var(--color-orange)",
+    },
     extend: {
       fontFamily: {
         sans: ["Rubik", ...fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            a: {
+              color: theme("colors.orange"),
+            },
+            h1: {
+              color: theme("colors.gray.700"),
+            },
+            code: { color: theme("colors.blue.500") },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+      }),
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
