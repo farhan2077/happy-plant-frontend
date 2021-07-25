6c059dfc7325f2ca8903ae71b8b370d0807d53d5
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-25 11:05:28 +0600

    fix: certain colors not being generated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      orange: "var(--color-orange)",
-    },
     extend: {
+      colors: {
+        orange: "var(--color-orange)",
+      },
       fontFamily: {
         sans: ["Rubik", ...fontFamily.sans],
       },
