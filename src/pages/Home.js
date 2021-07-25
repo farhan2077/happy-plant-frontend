85d32acf64582482888b99d641dd413c22f60752
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-25 15:33:45 +0600

    test: demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,8 @@ import { agent } from "../helpers/agent";
 import happyPlant from "../assets/happy-plant.svg";
 import sadPlant from "../assets/sad-plant.svg";
 
-let upperValue = 600; // limit for values from moisture sensor
-let lowerValue = 300;
+let upperValue = 350; // limit for values from moisture sensor
+let lowerValue = 150;
 let intervalValue = 60 * 30; // unit in second
 
 export default function Home() {
