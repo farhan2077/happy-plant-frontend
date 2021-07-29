a8344ba95c52fbf93eb5fe7421899a9bb0076188
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-29 18:23:48 +0600

    new: updated home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import happyPlant from "../assets/happy-plant.svg";
 import sadPlant from "../assets/sad-plant.svg";
 
 let upperValue = 600; // limit for values from moisture sensor
-let lowerValue = 400;
+let lowerValue = 300;
 let intervalValue = 60 * 30; // unit in second
 
 export default function Home() {
@@ -45,26 +45,29 @@ export default function Home() {
               <div className="w-64 sm:w-80">
                 {plantData.data[0].reading < upperValue &&
                 plantData.data[0].reading > lowerValue ? (
-                  <img
-                    src={happyPlant}
-                    alt="Happy plant"
-                    className="w-full h-auto -mt-32 sm:-mt-10"
-                  />
+                  <>
+                    <img
+                      src={happyPlant}
+                      alt="Happy plant"
+                      className="w-full h-auto -mt-32 sm:-mt-10"
+                    />
+                    <p className="mt-3 text-xl">Your plant is happy 😀.</p>
+                  </>
                 ) : (
-                  <img
-                    src={sadPlant}
-                    alt="Sad Plant"
-                    className="w-full h-auto"
-                  />
+                  <div>
+                    <img
+                      src={sadPlant}
+                      alt="Sad Plant"
+                      className="w-full h-auto"
+                    />
+                    <p className="mt-3 text-xl">Your plant is sad 😢.</p>
+                  </div>
                 )}
               </div>
-              <p className="mt-3 text-xl">
-                Soil moisture is {plantData.data[0].reading}
-              </p>
             </div>
           ) : (
             <div className="mx-10 text-xl">
-              Can not display plant status 😢.
+              Can not display plant status.
               <br />
               Check if everything is working or try again later.
             </div>
