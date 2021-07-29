9eb17118286f24de4033d6e99aac4132a1904a89
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-29 18:23:17 +0600

    refactor: updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,21 +6,25 @@ export default function About() {
   return (
     <>
       <MetaDecorator title="Happy Plant - About" />
-      <section className="mx-auto mt-6 prose">
-        <h1>
+      <section className="px-0 mt-5 text-gray-700 md:px-14">
+        <h1 className="text-xl font-bold sm:text-4xl">
           Everybody asks "What is the plant?" but nobody asks "How is the
           plant?"
         </h1>
-        <p>
+        <p className="my-6">
           That's why I, Farhan with Sakib and Nur Amin did this project that
           shows if a plant is happy or sad.
         </p>
-        <p>
+        <p className="my-6">
           Just kidding. We did this as a project showcase for our 5th term's
-          course "Microprocessor and Embedded Systems".
+          course called "Microprocessor and Embedded Systems".
         </p>
         <p>
-          See <Link to="/how">how it works</Link>.
+          See{" "}
+          <Link className="font-medium underline text-orange" to="/how">
+            how it works
+          </Link>
+          .
         </p>
       </section>
     </>
